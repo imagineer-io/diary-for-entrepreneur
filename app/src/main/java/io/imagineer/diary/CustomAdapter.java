2b607ec78c607cd3814ebffd4ecf2048b7ea07ba
Minsoo Kwak <marcosdeseul@gmail.com>
2017-05-20 14:06:01 +0800

    Add CustomAdapter

---
+++ app/src/main/java/io/imagineer/diary/CustomAdapter.java
@@ -0,0 +1,44 @@
+package io.imagineer.diary;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marco on 5/20/17.
+ */
+
+public class CustomAdapter extends ArrayAdapter<Article> {
+    private Context mContext;
+    private ArrayList<Article> mArticles;
+
+    public CustomAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Article> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        this.mArticles = (ArrayList<Article>) objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.list_row, parent, false);
+
+        TextView titleView = (TextView) row.findViewById(R.id.titleView);
+        TextView contentView = (TextView) row.findViewById(R.id.contentView);
+
+        titleView.setText(mArticles.get(position).getTitle());
+        contentView.setText(mArticles.get(position).getContent());
+
+        return row;
+    }
+}
--- app/src/main/java/io/imagineer/diary/ListActivity.java
+++ app/src/main/java/io/imagineer/diary/ListActivity.java
@@ -8,6 +8,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ListActivity extends AppCompatActivity {
 
     private ListView mListView;
@@ -25,14 +28,19 @@ public class ListActivity extends AppCompatActivity {
         };
 
         mListView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(this, R.layout.list_item, sampleArray);
+        // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, sampleArray);
+        CustomAdapter adapter = new CustomAdapter(
+                this,
+                R.layout.list_row,
+                new ArrayList<Article>(Arrays.asList(articles)));
+
         mListView.setAdapter(adapter);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(ListActivity.this, MainActivity.class);
-                intent.putExtra("item", sampleArray[position]);
+                // TODO: fix intent extra
+                // intent.putExtra("item", sampleArray[position]);
                 startActivity(intent);
             }
         });
